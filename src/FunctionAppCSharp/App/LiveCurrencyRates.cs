2a0e9cdb189169e5c45c1e91f641ddea9290bb69
Karen Tazayan <karen.tazayan@outlook.com>
2019-06-19 12:36:06 +0400

    Added local.settings.json into Azure Function projects.

---
+++ src/FunctionAppCSharp/App/LiveCurrencyRates.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Microsoft.Build.Utilities;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppCSharp
+{
+    public class LiveCurrencyRates
+    {
+        [FunctionName("RefreshLiveCurrencyRates")]
+        public void RefreshLiveCurrencyRates(
+            [TimerTrigger("*/5 * * * * *")]TimerInfo timerInfo,
+            [SignalR(HubName = "CurrencyRates")] IAsyncCollector<SignalRMessage> signalrMessages,
+            Logger log)
+        {
+            var currentDate = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            var message = "Message from MessageDispatcher: " + currentDate;
+
+
+            log.LogInformation($"C# Timer trigger function executed at: {currentDate}");
+        }
+    }
+}
